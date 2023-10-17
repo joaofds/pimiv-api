00398d4debd3c55db5e8dac2cb1de74a977250e6
Joao <jo4o.fds@gmail.com>
2023-10-16 23:04:42 -0300

    models

--- FolhaPagamento.API/Models/FolhaPagamentoContext.cs
+++ FolhaPagamento.API/Models/FolhaPagamentoContext.cs
@@ -8,26 +8,79 @@ namespace Models;
 
 public partial class FolhaPagamentoContext : DbContext
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public FolhaPagamentoContext()
     {
-        var connectionString = "Server=127.0.0.1;Database=folha_pagamento;Uid=root;Pwd=root;";
-        var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
-        optionsBuilder.UseMySql(connectionString, serverVersion);
     }
+
+    public FolhaPagamentoContext(DbContextOptions<FolhaPagamentoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beneficio> Beneficios { get; set; }
+
+    public virtual DbSet<Cargo> Cargos { get; set; }
+
+    public virtual DbSet<Colaborador> Colaboradores { get; set; }
+
+    public virtual DbSet<ColaboradorBeneficio> ColaboradoresBeneficios { get; set; }
+
+    public virtual DbSet<Endereco> Enderecos { get; set; }
+
+    public virtual DbSet<FolhaPagamento> FolhaPagamentos { get; set; }
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=LOCALHOST;Initial Catalog=FolhadePagamento;Persist Security Info=True;User ID=sa;Password=root");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder
-            .UseCollation("utf8mb4_0900_ai_ci")
-            .HasCharSet("utf8mb4");
+        modelBuilder.Entity<Beneficio>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_beneficios_id");
+        });
+
+        modelBuilder.Entity<Cargo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_cargos_id");
+        });
+
+        modelBuilder.Entity<Colaborador>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_colaboradores_id");
+
+            entity.HasOne(d => d.Cargo).WithMany(p => p.Colaboradores).HasConstraintName("FK_colaboradores_cargo_id__cargos_id");
+        });
+
+        modelBuilder.Entity<ColaboradorBeneficio>(entity =>
+        {
+            entity.HasOne(d => d.Beneficio).WithMany().HasConstraintName("FK_colaboradores_beneficios_beneficio_id__beneficios_id");
+
+            entity.HasOne(d => d.Colaborador).WithMany().HasConstraintName("FK_colaboradores_beneficios_colaborador_id__colaboradores_id");
+        });
+
+        modelBuilder.Entity<Endereco>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_enderecos_id");
+
+            entity.Property(e => e.Uf).IsFixedLength();
+
+            entity.HasOne(d => d.Colaborador).WithMany(p => p.Enderecos).HasConstraintName("FK_enderecos_colaborador_id__colaboradores_id");
+        });
+
+        modelBuilder.Entity<FolhaPagamento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_folha_pagamentos_id");
+
+            entity.HasOne(d => d.Colaborador).WithMany(p => p.FolhaPagamentos).HasConstraintName("FK_folha_pagamentos_colaborador_id__colaborador_id");
+        });
 
         modelBuilder.Entity<Usuario>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
+            entity.HasKey(e => e.Id).HasName("PK_usuarios_id");
 
-            entity.ToTable("usuarios", tb => tb.HasComment("tabela de usuarios"));
+            entity.Property(e => e.Id).ValueGeneratedNever();
         });
 
         OnModelCreatingPartial(modelBuilder);
--- FolhaPagamento.API/Models/Usuario.cs
+++ FolhaPagamento.API/Models/Usuario.cs
@@ -8,37 +8,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Models;
 
-/// <summary>
-/// tabela de usuarios
-/// </summary>
 [Table("usuarios")]
-[Index("Email", Name = "email_UNIQUE", IsUnique = true)]
-[Index("Id", Name = "id_UNIQUE", IsUnique = true)]
 public partial class Usuario
 {
     [Key]
     [Column("id")]
-    public uint Id { get; set; }
+    public int Id { get; set; }
 
-    [Required]
     [Column("nome")]
-    [StringLength(100)]
+    [StringLength(200)]
+    [Unicode(false)]
     public string Nome { get; set; }
 
     [Column("sobrenome")]
     [StringLength(200)]
+    [Unicode(false)]
     public string Sobrenome { get; set; }
 
-    [Required]
     [Column("email")]
     [StringLength(100)]
+    [Unicode(false)]
     public string Email { get; set; }
 
-    [Required]
     [Column("senha")]
     [StringLength(255)]
+    [Unicode(false)]
     public string Senha { get; set; }
 
-    [Column("created_at", TypeName = "datetime")]
-    public DateTime? CreatedAt { get; set; }
+    [Column("data_registro", TypeName = "datetime")]
+    public DateTime? DataRegistro { get; set; }
 }
