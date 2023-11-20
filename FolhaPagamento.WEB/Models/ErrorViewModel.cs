de9e5aa7f59e2c374916334c8823b61e8f7a5159
Joao <jo4o.fds@gmail.com>
2023-11-20 14:34:53 -0300

    web app mvc

---
+++ FolhaPagamento.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FolhaPagamento.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
