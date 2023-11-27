4a4b95149d5ff9c0e2f233b004b20a2796168909
Joao <jo4o.fds@gmail.com>
2023-11-27 18:45:17 -0300

    cadastro e listagem usuarios

---
+++ FolhaPagamento.WEB/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using FolhaPagamento.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FolhaPagamento.WEB.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("usuario/list")]
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
