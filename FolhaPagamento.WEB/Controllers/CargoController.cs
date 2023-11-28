90ae82ce03e23b778f90f13227843ac4ddb3cd54
Joao <jo4o.fds@gmail.com>
2023-11-27 23:14:37 -0300

    cadastro e listagem cargos

---
+++ FolhaPagamento.WEB/Controllers/CargoController.cs
@@ -0,0 +1,26 @@
+﻿using FolhaPagamento.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FolhaPagamento.WEB.Controllers
+{
+    public class CargoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("cargo/list")]
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
